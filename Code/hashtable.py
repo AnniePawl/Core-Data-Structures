958e8e2128c48648bc3285231b31cd5f94ec8bde
Anna Pawl <annampawl@gmail.com>
2020-05-06 17:56:11 -0400

    hash tables tests passing

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -27,7 +27,7 @@ class HashTable(object):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
         # TODO: Calculate load factor
-        # return ...
+        return float(self.size)/len(self.buckets)
 
     def keys(self):
         """Return a list of all keys in this hash table.
@@ -71,8 +71,8 @@ class HashTable(object):
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        Best case running time: O(1) 
+        Worst case running time: O(n) """
         # Find the bucket the given key belongs in
         index = self._bucket_index(key)
         bucket = self.buckets[index]
@@ -111,13 +111,16 @@ class HashTable(object):
             # In this case, the given key's value is being updated
             # Remove the old key-value entry from the bucket first
             bucket.delete(entry)
+            self.size-=1
         # Insert the new key-value entry into the bucket in either case
         bucket.append((key, value))
+        self.size += 1
         # TODO: Check if the load factor exceeds a threshold such as 0.75
-        # ...
-        # TODO: If so, automatically resize to reduce the load factor
-        # ...
+        # ...If so, automatically resize to reduce the load factor
+        if self.load_factor() > .75:
+            self._resize()
 
+    
     def delete(self, key):
         """Delete the given key and its associated value, or raise KeyError.
         Best case running time: ??? under what conditions? [TODO]
@@ -130,6 +133,7 @@ class HashTable(object):
         if entry is not None:  # Found
             # Remove the key-value entry from the bucket
             bucket.delete(entry)
+            self.size -= 1
         else:  # Not found
             raise KeyError('Key not found: {}'.format(key))
 
@@ -146,12 +150,17 @@ class HashTable(object):
         elif new_size is 0:
             new_size = len(self.buckets) / 2  # Half size
         # TODO: Get a list to temporarily hold all current key-value entries
-        # ...
+        temp_entries = self.items()
+    
         # TODO: Create a new list of new_size total empty linked list buckets
+        self.__init__(new_size)
         # ...
         # TODO: Insert each key-value entry into the new list of buckets,
         # which will rehash them into a new bucket index based on the new size
-        # ...
+        for entry in temp_entries:
+            key = entry[0]
+            value = entry[1]
+            self.set(key,value) # O(l) l--> length of ll
 
 
 def test_hash_table():
